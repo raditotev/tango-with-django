2984641ccb985ab9c66845ef252322f62878a2f7
Radi Totev <raditotev@gmail.com>
2015-06-15 12:23:54 +0100

    Creating static files folder and updating URL for static files.

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+STATIC_PATH = os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -90,4 +91,8 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     TEMPLATE_PATH,
+)
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
 )
