9da470e7cf967f094d75f76ae7f391b2930ebd3c
Radi Totev <raditotev@gmail.com>
2015-06-18 11:33:29 +0100

    Restricted access and logout

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -85,6 +85,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+LOGIN_URL = '/rango/login/'
+
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
