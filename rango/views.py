bed97e1853369ef829792b087b9c4fb9fb482532
Radi Totev <raditotev@gmail.com>
2015-06-24 15:08:45 +0100

    Added Page views counter - track_url in rango/views.py

--- rango/views.py
+++ rango/views.py
@@ -146,3 +146,15 @@ def search(request):
             result_list = run_query(query)
 
     return render(request, 'rango/search.html', {'result_list': result_list})
+
+def track_url(request):
+    if request.method == 'GET':
+        if 'page_id' in request.GET:
+            page_id = request.GET['page_id']
+            page = Page.objects.get(id=page_id)
+            page.views += 1
+            page.save()
+            return HttpResponseRedirect(page.url)
+    else:
+        return index(request)
+
