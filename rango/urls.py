6a6d55a23bdfe8fe1dc85a73389bfafb3702504a
Radi Totev <raditotev@gmail.com>
2015-06-29 16:49:27 +0100

    Added like button for Category

--- rango/urls.py
+++ rango/urls.py
@@ -13,4 +13,5 @@ urlpatterns = patterns('',
         url(r'^profile/$', views.profile, name='profile'),
         url(r'^users/$', views.users, name='users'),
         url(r'^users/(?P<username>[\w\-]+)/$', views.user_profile, name='user_profile'),
+        url(r'^like_category/$', views.like_category, name='like_category'),
         )
