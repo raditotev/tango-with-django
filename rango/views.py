af94ed5b936614e001d617d484a7a7477802cf5d
Radi Totev <raditotev@gmail.com>
2015-06-29 12:11:12 +0100

    Modified category view to search for category name when there is no query.

--- rango/views.py
+++ rango/views.py
@@ -77,18 +77,7 @@ def about(request):
 def category(request, category_name_slug):
 
     context_dict = {}
-
-    try:
-        category = Category.objects.get(slug=category_name_slug)
-        context_dict['category_name'] = category.name
-
-        pages = Page.objects.filter(category=category)
-
-        context_dict['pages'] = pages
-        context_dict['category'] = category
-        context_dict['category_name_slug'] = category_name_slug
-    except Category.DoesNotExist:
-        pass
+    context_dict['query'] = None
 
     result_list = []
     # Search request
@@ -99,6 +88,22 @@ def category(request, category_name_slug):
             # Run our Bing function to get the results list!
             result_list = run_query(query)
             context_dict['result_list'] = result_list
+            context_dict['query'] = query
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        pages = Page.objects.filter(category=category).order_by('-views')
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+        context_dict['category_name_slug'] = category_name_slug
+    except Category.DoesNotExist:
+        pass
+
+    if not context_dict['query']:
+        context_dict['query'] = category.name
 
     return render(request, 'rango/category.html', context_dict)
 
